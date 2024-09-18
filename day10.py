7328d8a6ceb5761a868a12fcec4db48c21048f74
Sara030204 <shaikhsara0302@gmail.com>
2024-09-18 22:11:38 +0530

    Day10 task completed

---
+++ day10.py
@@ -0,0 +1,19 @@
+#  group all the strings that are anagrams of each other
+
+strs= ["abc", "def", "ghi"]
+group_anagrams=[]
+
+while strs:
+    s=strs[0]
+    characters=sorted(list(s))
+    anagram=[]
+    for j in strs:
+        if sorted(list(j))==characters:
+            anagram.append(j)
+    group_anagrams.append(anagram)
+
+    for i in anagram:
+        strs.remove(i)
+
+print(group_anagrams)
+    
